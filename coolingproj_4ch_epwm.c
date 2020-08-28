b1f80d9a9251d59c1d544355cee9b24c5d831719
Xiaofan Cui <richardcuixf@gmail.com>
2020-08-27 20:48:50 -0400

    Update coolingproj_4ch_epwm.c
    Change the comments for logging

--- coolingproj_4ch_epwm.c
+++ coolingproj_4ch_epwm.c
@@ -1,8 +1,8 @@
 //#############################################################################
 //
-// FILE:   epwm_ex2_updown_aq.c
+// FILE:   coolingproj_4ch_epwm.c
 //
-// TITLE:  ePWM Action Qualifier Module - Using up/down count.
+// TITLE:  based on ePWM Action Qualifier Module - Using up count / 4 modules synchronziation
 //
 //! \addtogroup driver_example_list
 //! <h1> ePWM Up Down Count Action Qualifier</h1>
@@ -12,45 +12,16 @@
 //!
 //! The compare values CMPA and CMPB are modified within the ePWM's ISR.
 //!
-//! The TB counter is in up/down count mode for this example.
+//! The TB counter is in up count mode for this example.
 //!
 //! View the ePWM1A/B(GPIO0 & GPIO1), ePWM2A/B(GPIO2 &GPIO3)
 //! and ePWM3A/B(GPIO4 & GPIO5) waveforms on oscilloscope.
 //
 //#############################################################################
 // $TI Release: F2837xD Support Library v3.10.00.00 $
-// $Release Date: Tue May 26 17:13:46 IST 2020 $
+// $Release Date: 08/27 IST 2020 $
 // $Copyright:
-// Copyright (C) 2013-2020 Texas Instruments Incorporated - http://www.ti.com/
-//
-// Redistribution and use in source and binary forms, with or without 
-// modification, are permitted provided that the following conditions 
-// are met:
-// 
-//   Redistributions of source code must retain the above copyright 
-//   notice, this list of conditions and the following disclaimer.
-// 
-//   Redistributions in binary form must reproduce the above copyright
-//   notice, this list of conditions and the following disclaimer in the 
-//   documentation and/or other materials provided with the   
-//   distribution.
-// 
-//   Neither the name of Texas Instruments Incorporated nor the names of
-//   its contributors may be used to endorse or promote products derived
-//   from this software without specific prior written permission.
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
-// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
-// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
-// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// $
+// Xiaofan Cui
 //#############################################################################
 
 //
